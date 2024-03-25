dc61768056b6f68253c92704ad11e6ed5642621b
sugutlynn <sugutlynn@gmail.com>
2024-03-25 16:44:24 +0300

    Added Signup & Login function

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
-import { Suspense, useEffect } from "react"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { Suspense } from "react"
 import { routes } from './routes'
 
 function App() {
